ee2cfa5d5cb80f9af03dad6f1424a0536ed6cf3a
Leandro Tavano <tavanoleandro@gmail.com>
2026-03-03 14:02:47 -0300

    Container e Home

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/Container"
+export default function Dashboard() {
+    return (
+        <Container>
+            <div className="">
+                Dashboardddd
+            </div>
+        </Container>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import heroImg from '@/assets/hero.svg';
+
 
 export default function Home() {
   return (
     <div className="">
-      <main className="">
-
+      <main className="w-full flex flex-col justify-center items-center min-h-[calc(100vh-80px)]">
+        <h2 className="font-medium text-2xl">Gerencie sua Empresa</h2>
+        <h3 className="font-bold text-blue-500 text-3xl mb-6">Atendimentos e Clientes</h3>
+        <Image 
+        src={heroImg}
+        alt="Logo"
+        width={600}
+        className="max-w-sm md:max-w-xl" />
       </main>
     </div>
   );
